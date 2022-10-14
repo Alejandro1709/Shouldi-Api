e89a9344994b2817cbed940b2ead27b11d718133
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-10-14 17:46:04 -0500

    Remove: process.exit

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,6 @@ export const connectDb = async (uri) => {
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error(error);
-    process.exit(1);
+    // process.exit(1);
   }
 };
