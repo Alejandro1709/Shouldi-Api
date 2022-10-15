421f96073fe3a62b5307ca34bd35a8ae7429b865
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-10-15 15:12:06 -0500

    Add: errors in dev and prod

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,5 +1,33 @@
 import AppError from '../utils/AppError.js';
 
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak details to client
+  } else {
+    // 1. Log error to console
+    console.error('ERROR 🔥', err);
+    // 2. Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 export const notFound = (req, res, next) => {
   next(new AppError('This route does not exists!', 404));
 };
@@ -8,8 +36,9 @@ export const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
