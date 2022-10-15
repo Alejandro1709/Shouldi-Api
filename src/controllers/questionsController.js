41652ea6212996a235a13ce0ff0157b71b432602
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-10-15 13:13:44 -0500

    Add: update and delete question endpoints

--- src/controllers/questionsController.js
+++ src/controllers/questionsController.js
@@ -22,3 +22,28 @@ export const createQuestion = async (req, res) => {
 
   res.status(200).json(question);
 };
+
+export const updateQuestion = async (req, res) => {
+  const { title, content, upvotes, downvotes } = req.body;
+
+  const question = await Question.findOneAndUpdate(
+    { slug: req.params.slug },
+    {
+      title,
+      content,
+      upvotes,
+      downvotes,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json(question);
+};
+
+export const deleteQuestion = async (req, res) => {
+  const question = await Question.findOneAndRemove({ slug: req.params.slug });
+  req.status(200).json({ message: 'Question Removed!' });
+};
