a52fe3dbe35af4fc428445795f69942ce73ede93
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-10-17 14:29:27 -0500

    Add: error handling for JWT

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,8 @@
+import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/AppError.js';
 
 export const protect = catchAsync(async (req, res, next) => {
   let token;
@@ -12,9 +14,24 @@ export const protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   const user = await User.findById(decoded.id).select('-password');
+
+  if (!user) {
+    return next(
+      new AppError(
+        'The user belonging to this token does not longer exists.',
+        401
+      )
+    );
+  }
 
   req.user = user;
 
